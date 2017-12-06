77a6f657cde414b6b613792f762231043afcda55
Amber <proquestionasker@gmail.com>
2017-12-06 11:00:42 -0800

    Simplifying init

--- init.R
+++ init.R
@@ -1,15 +1,2 @@
-# init.R
-#
-# Example R code to install packages if not already installed
-#
-
-my_packages = c("dplyr", "purrr", "darksky")
-
-install_if_missing = function(p) {
-  if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p)
-  }
-}
-
-invisible(sapply(my_packages, install_if_missing))
-
+install.packages('tidyverse')
+install.packages('darksky')
