6157f9f48a728b73ce954d259f5d72729c28fad4
Amber <proquestionasker@gmail.com>
2017-12-05 23:44:08 -0800

    Initial commit

---
+++ init.R
@@ -0,0 +1,39 @@
+# init.R
+#
+# Example R code to install packages if not already installed
+#
+
+my_packages = c("tidyverse", "darksky")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
+
+cityLocations <- read.csv("cityLocations.csv", stringsAsFactors = FALSE, header = TRUE)
+
+getWeather <- function(citySelect, territorySelect){
+  coordinates <- cityLocations %>% 
+    filter(City == citySelect & Territory == territorySelect)
+  
+  weather <- get_current_forecast(coordinates$latitude, coordinates$longitude, exclude = c("currently,minutely,hourly,alerts,flags")) 
+  
+  weather2 <- weather$daily %>% 
+    mutate(city = citySelect,
+           territory = territorySelect,
+           latitude = coordinates$latitude,
+           longitude = coordinates$longitude,
+           day = row_number()) %>%
+    filter(day == 1) %>% 
+    select(c('city', 'territory', 'latitude', 'longitude', 'time', 'summary', 'temperatureMin', 'temperatureMax', 'windSpeed', 'pressure'))
+}
+
+safeWeather <- possibly(getWeather, otherwise = NA)
+
+#todayWeather <- map2_dfr(cityLocations$City, cityLocations$Territory, safeWeather)
+todayWeather <- map2_dfr(testList$City, testList$Territory, safeWeather)
+
+write.csv(todayWeather, "todayWeather.csv", row.names = FALSE)
